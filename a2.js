e97445f49eb9d633d13f896080f845c0b1b3531c
PMarie <105440976+PMarieMEM@users.noreply.github.com>
2022-06-02 16:42:09 -0400

    First commit for assignment 2

---
+++ a2.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+*  WEB700 – Assignment 2
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  
+*  No part of this assignment has been copied manually or electronically from any other source
+*  (including web sites) or distributed to other students.
+* 
+*  Name: PRECIOUS MARIE E. MALINNAG Student ID: 147010219 Date: JUNE 1, 2022
+*
+********************************************************************************/ 
+
+console.log("This is a test")
